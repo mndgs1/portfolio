e542aa03c004fb04104fc779fd50ac268c00594b
mndgs1 <mindaugasbankauskas@gmail.com>
2024-05-01 21:53:10 +0200

    Added about section added personal photos

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,13 @@ import {
     CarouselPrevious,
 } from "@/components/ui/carousel";
 import { H2, H3 } from "@/components/typography/Headings";
+import { P } from "@/components/typography/Paragraph";
 import FeaturedProjectCard from "@/components/shared/FeaturedProjectCard";
 
 import LinkWithIcon from "@/components/shared/LinkWithIcon";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinode } from "@fortawesome/free-brands-svg-icons";
+import Image from "next/image";
 
 export default function Home() {
     return (
@@ -46,8 +48,39 @@ export default function Home() {
                     Node
                 </LinkWithIcon>
             </section>
-            <section className="About me">
+            <section className="About me section">
                 <H2>About me</H2>
+                {/* <div className="md:grid md:grid-cols-2 md:justify-items-right md:items-center"> */}
+                <div className="flex flex-col lg:flex-row xl:mx-24 lg:gap-20 items-center">
+                    <div className="lg:order-2 mb-6">
+                        <P className="md:mb-6">
+                            Hey there! I'm [Your Name], a 31-year-old with a
+                            passion for building both physical and digital
+                            worlds. For over a decade, I've wielded my tools as
+                            a carpenter, crafting structures and spaces that
+                            stand the test of time. But as life evolved, so did
+                            my interests.
+                        </P>
+                        <P>
+                            Today, I'm not just a carpenter; I'm a family man.
+                            My world revolves around my wonderful wife, two
+                            energetic boys, and our loyal canine companion. They
+                            bring joy, chaos, and a whole lot of love into every
+                            corner of our lives.
+                        </P>
+                    </div>
+                    <div className="">
+                        <div className="bg-green-500 w-96 h-96 rounded-full">
+                            <Image
+                                src="/img/personal/family_photo.jpeg"
+                                alt="personal photo"
+                                width={800}
+                                height={800}
+                                className="rounded-full w-full h-full object-cover"
+                            />
+                        </div>
+                    </div>
+                </div>
             </section>
         </>
     );
