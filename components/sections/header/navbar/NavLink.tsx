da424591e86487120270e5be340078f5daf9cf3f
mndgs1 <mindaugasbankauskas@gmail.com>
2024-05-16 15:14:41 +0200

    Fixed a typescript bug

--- components/sections/header/navbar/NavLink.tsx
+++ components/sections/header/navbar/NavLink.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-interface NavLinkProps extends React.ReactElement {
+interface NavLinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
     href: string;
     label: string;
     pathname: string;
@@ -8,14 +8,15 @@ interface NavLinkProps extends React.ReactElement {
 
 const NavLink = ({ href, label, pathname, ...rest }: NavLinkProps) => {
     return (
-        <li className="group" {...rest}>
+        <li className="group">
             <Link
                 href={href}
                 className={`md:text-base w-max m-auto font-medium dark:font-normal group-hover:text-foreground transition-all flex flex-col items-center ${
                     pathname === href
                         ? "text-foreground"
                         : "text-muted-foreground"
-                }`}>
+                }`}
+                {...rest}>
                 {label}
                 <div
                     className={`bg-primary group-hover:w-full h-[1px] transition-all duration-300 ${
